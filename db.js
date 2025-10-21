6547172035565a84dc5d6e8518f435c8cf159220
webcreation-dev <adjilan2403@gmail.com>
2025-10-21 13:26:06 +0100

    review config database

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'asterisk',
-  host: 'localhost',
-  database: 'asterisk',
-  password: 'Obelix',
-  port: 5432,
+  user: process.env.DB_USER || 'asterisk',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'asterisk',
+  password: process.env.DB_PASSWORD || 'Obelix',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = {
