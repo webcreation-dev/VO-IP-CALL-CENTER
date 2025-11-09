46e3ee0b561d3d6ead7a509736dd9d1756612873
webcreation-dev <adjilan2403@gmail.com>
2025-11-09 16:19:24 +0100

    fix bugs

--- asterisk-api-v2/src/queues/queues.module.ts
+++ asterisk-api-v2/src/queues/queues.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QueuesController } from './queues.controller';
 import { QueuesService } from './queues.service';
 import { Queue } from './entities/queue.entity';
+import { QueueMember } from '../queue-members/entities/queue-member.entity';
 import { Extension } from '../core/database/entities/extension.entity';
 import { Tenant } from '../core/database/entities/tenant.entity';
 import { TenantContext } from '../core/database/entities/tenant-context.entity';
@@ -14,7 +15,7 @@ import { SoundsModule } from '../sounds/sounds.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Queue, Extension, Tenant, TenantContext]),
+    TypeOrmModule.forFeature([Queue, QueueMember, Extension, Tenant, TenantContext]),
     AmiModule,
     CacheModule,
     forwardRef(() => TenantsModule),
